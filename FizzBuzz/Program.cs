54edee6e026356a503f059d7daf07ddff016875b
SaymanDotNet <saymannsk@gmail.com>
2025-03-14 12:22:47 +0700

    Добавьте файлы проекта.

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] numbers = Enumerable.Range(1, 100).ToArray();
+
+
+foreach (var number in numbers)
+{
+	string word = "";
+
+	if(number % 3 == 0) word += "Fizz";
+	if(number % 5 == 0) word += "Buzz";
+
+	if(string.IsNullOrEmpty(word)) word = number.ToString();
+
+	Console.WriteLine(word);
+}
+
+Console.ReadLine();
